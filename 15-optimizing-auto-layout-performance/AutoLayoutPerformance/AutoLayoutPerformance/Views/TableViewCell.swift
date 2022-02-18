8a02bd020bb310933a7d2e566e7edeb46a46eb29
Shaun Fowler <shaunfowler@users.noreply.github.com>
2022-02-17 18:53:58 -0800

    ch15. optimizing auto layout

--- 15-optimizing-auto-layout-performance/AutoLayoutPerformance/AutoLayoutPerformance/Views/TableViewCell.swift
+++ 15-optimizing-auto-layout-performance/AutoLayoutPerformance/AutoLayoutPerformance/Views/TableViewCell.swift
@@ -1,34 +1,7 @@
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import UIKit
 
 final class TableViewCell: UITableViewCell {
+
   // MARK: - Properties
   private var beverage: Beverage? {
     didSet {
@@ -44,9 +17,10 @@ final class TableViewCell: UITableViewCell {
     }
   }
   
+  private var layoutConstraints: [NSLayoutConstraint] = []
   private var staticConstraints: [NSLayoutConstraint] = []
   private var dynamicConstraints: [NSLayoutConstraint] = []
-  
+
   private let usernameLabel: UILabel = {
     let label = UILabel()
     label.translatesAutoresizingMaskIntoConstraints = false
@@ -196,8 +170,16 @@ final class TableViewCell: UITableViewCell {
   }
   
   // MARK: - Constraints
+  private func updateDynamicConstraints(isHighCalorie: Bool) {
+    NSLayoutConstraint.deactivate(dynamicConstraints)
+    dynamicConstraints = isHighCalorie ? highCalorieConstraints : lowCalorieConstraints
+    NSLayoutConstraint.activate(dynamicConstraints)
+  }
+
   override func updateConstraints() {
     print("Update constraints")
+
+    // Only activate static constraints once
     if staticConstraints.isEmpty {
       staticConstraints =
         usernameLabelConstraints
@@ -207,20 +189,14 @@ final class TableViewCell: UITableViewCell {
         + postImageViewConstraints
       NSLayoutConstraint.activate(staticConstraints)
     }
-    
-    if beverage?.isHighCalorie ?? false
-      && dynamicConstraints != highCalorieConstraints {
+
+    // Only activate dynamic constraints if they are different than currently applied dynamic constraints
+    if beverage?.isHighCalorie ?? false && dynamicConstraints != highCalorieConstraints {
       updateDynamicConstraints(isHighCalorie: true)
     } else if dynamicConstraints != lowCalorieConstraints {
       updateDynamicConstraints(isHighCalorie: false)
     }
+
     super.updateConstraints()
-  }
-  
-  private func updateDynamicConstraints(isHighCalorie: Bool) {
-    NSLayoutConstraint.deactivate(dynamicConstraints)
-    dynamicConstraints = isHighCalorie
-      ? highCalorieConstraints : lowCalorieConstraints
-    NSLayoutConstraint.activate(dynamicConstraints)
   }
 }
